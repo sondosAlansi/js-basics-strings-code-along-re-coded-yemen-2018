8cc115aa8452f6c7f3371785b23c7ac131cbef8e
sondos alansi <sondos.alansi@yahoo.com>
2018-10-15 19:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Write your code in this file!
+<<<<<<< HEAD
   
+=======
+>>>>>>> acb708f0c52dce332de20a1dfbe3cb2174d92bce
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
